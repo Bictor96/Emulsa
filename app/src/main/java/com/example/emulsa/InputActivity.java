01e396a4c4be277a7d78868f2a587d0cdc133f0e
DESKTOP-GP5MR98\bicto <bictor96@gmail.com>
2020-10-09 14:03:18 +0200

    Añadida funcionalidad a los botones de Abrir Galeria y Sacar Foto

--- app/src/main/java/com/example/emulsa/InputActivity.java
+++ app/src/main/java/com/example/emulsa/InputActivity.java
@@ -1,21 +1,49 @@
 package com.example.emulsa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class InputActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_IMAGE_GALLERY = 2;
 
+    private String currentPhotoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView selectedImageView = (ImageView) findViewById(R.id.selectedImageView);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            selectedImageView.setImageURI(Uri.parse(currentPhotoPath));
+        }
+
+        else if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == RESULT_OK) {
+            selectedImageView.setImageURI(data.getData());
+        }
     }
 
     public void OpenGallery(View view) {
@@ -29,9 +57,35 @@ public class InputActivity extends AppCompatActivity {
     }
 
     public void OpenCamera(View view) {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Comprueba que la camara esta activa
+        if (takePhotoIntent.resolveActivity(getPackageManager()) != null) {
+            // Crea el File donde ira la foto.
+            File photoFile = null;
+            try {
+                photoFile = CreateImageFile();
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
+    }
+
+    private File CreateImageFile() throws IOException {
+        // Crear el nombre del fichero
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFilename = "JPEG_"+timestamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFilename, ".jpg", storageDir);
+
+        // Guarda y devuelve la ruta del archivo
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
